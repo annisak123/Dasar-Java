99fb3c7efa109034996951c9e9c200121e90fb61
annisak123 <102012607+annisak123@users.noreply.github.com>
2022-09-27 15:43:29 +0800

    Add files via upload

---
+++ Dasar java/src/Annisak.java
@@ -0,0 +1,25 @@
+public class Annisak {
+    public static void main(String[] args){
+        String namalengkap, alamat, jeniskelamin;
+        String tanggallahir;
+        int umur;
+
+        // mengisi variabel
+        namalengkap="Annisak";
+        alamat="kalisade";
+        jeniskelamin="Perempuan";
+        tanggallahir="17 Januari 2002";
+        umur=20;
+
+        System.out.println("\t===========================");
+        System.out.println("\t\tBIODATA SAYA");
+        System.out.println("\tNama Lengkap\t: "+namalengkap);
+        System.out.println("\talamat\t\t\t:" +alamat);
+        System.out.println("\tJenis Kelamin\t:"+jeniskelamin);
+        System.out.println("\ttanggal lahir\t: "+tanggallahir);
+        System.out.println("\tumur\t\t\t:"+umur);
+    }
+
+}
+
+
